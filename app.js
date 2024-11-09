1d7c83584412ea6e421ad29dac11076946f75d33
Shreyash Shukla <shreyashmanushukla419@gmail.com>
2024-11-09 07:12:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ document.getElementById("imageUpload").addEventListener("change", async function
     document.getElementById("loading").style.display = "block";
     document.getElementById("result").style.display = "none";
 
-    const apiKey = "MUyeyjtAaGaR8kRSwHLm6fPt"; // Your API key for Remove.bg
+    const apiKey = "f6CNzmqQDzp5GLwtUK9gwuGz"; // Your API key for Remove.bg
     const formData = new FormData();
     formData.append("image_file", file);
     formData.append("size", "auto");
